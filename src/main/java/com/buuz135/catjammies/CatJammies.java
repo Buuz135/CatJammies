a5cd0f720ec84cc50bd2c2e6bc69021510b0c55b
ChampionAsh5357 <championash5357@gmail.com>
2023-10-13 22:07:50 -0400

    Remove unnecessary code

--- src/main/java/com/buuz135/catjammies/CatJammies.java
+++ src/main/java/com/buuz135/catjammies/CatJammies.java
@@ -1,8 +1,6 @@
 package com.buuz135.catjammies;
 
-import net.minecraft.world.entity.EntityType;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
@@ -18,18 +16,11 @@ public class CatJammies {
 
 	public CatJammies() {
 		IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
-		DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> {
-			eventBus.addListener(this::doClientStuff);
-			eventBus.addListener(this::registerEntityRenderer);
-		});
+		DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> eventBus.addListener(this::doClientStuff));
 	}
 
 	private void doClientStuff(FMLClientSetupEvent event) {
 		IJammyDetector.DETECTORS.add(IJammyDetector.MUSIC);
 		IJammyDetector.DETECTORS.add(IJammyDetector.ME);
-	}
-
-	private void registerEntityRenderer(EntityRenderersEvent.RegisterRenderers event) {
-		event.registerEntityRenderer(EntityType.CAT, CatJammiesCatRenderer::new);
 	}
 }
